7287c40474cdecd5cc273896bf98c96404ed15e0
Patrick Goddard <thund3rbox@gmail.com>
2024-04-26 23:41:38 -0700

    add speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import "../../styles/main.scss";
 
 export const metadata: Metadata = {
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights/>
+      </body>
+      
     </html>
   );
 }
