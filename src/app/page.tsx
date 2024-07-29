3b955d54cfd1e2ce11ecf6b68526f0faa23aff58
Patrick Goddard <thund3rbox@gmail.com>
2024-07-29 07:19:07 -0700

    update contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,10 +74,18 @@ export default function Home() {
 
         </Section>
         <Section title="Contact">
-          <form name="contact" method="POST" data-netlify="true">
-            <label>Your Name: <input type="text" name="name" /></label>
-            <label>Your Email: <input type="email" name="email" /></label>
-            <label>Message: <textarea name="message"></textarea></label>
+          <form
+            action="https://formspree.io/f/xanwevpo"
+            method="POST"
+          >
+            <label>
+              Your email:
+              <input type="email" name="email" />
+            </label>
+            <label>
+              Your message:
+              <textarea name="message"></textarea>
+            </label>
             <button type="submit">Send</button>
           </form>
         </Section>
